f217bc836c6c8f6e723871cf4148dee5c3fa4101
Franco46-ochoa <46323573@fi.unju.edu.ar>
2026-06-26 03:29:39 -0300

    agrego modelo Zona

---
+++ src/models/Zona.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize');
+module.exports = (sequelize) => {
+const Zona = sequelize.define('Zona', {
+nombre: {
+type: DataTypes.STRING(100),
+allowNull: false,
+unique: true,
+},
+}, {
+tableName: 'zonas',
+timestamps: true,
+});
+return Zona;
+};

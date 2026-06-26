446fbf5dea98cc76b714ee56c433416eb61fd73f
Franco46-ochoa <46323573@fi.unju.edu.ar>
2026-06-26 03:25:53 -0300

    agrego modelo Usuario

---
+++ src/models/Usuario.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require("sequelize");
+module.exports = (sequelize) => {
+  const Usuario = sequelize.define(
+    "Usuario",
+    {
+      nombre: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(150),
+        allowNull: false,
+        unique: true,
+        validate: { isEmail: true },
+      },
+      password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      googleId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+      },
+      rol: {
+        type: DataTypes.ENUM("dueno", "gerente", "empleado"),
+        defaultValue: "empleado",
+      },
+      sector: {
+        type: DataTypes.ENUM("ventas", "finanzas", "stock"),
+        allowNull: true,
+      },
+      activo: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    {
+      tableName: "usuarios",
+      timestamps: true,
+    },
+  );
+  return Usuario;
+};

b9949568876ef3acc1767e548be5e6a880246b0b
Franco46-ochoa <46323573@fi.unju.edu.ar>
2026-06-30 03:42:18 -0300

    agregar CORS para permitir peticiones del frontend

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { connectDB } = require("./src/config/database");
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Rutas
